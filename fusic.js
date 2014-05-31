90aa0f38c389430fadff634b9d26674fcc9a5598
Pepf <pepijnfens@gmail.com>
2014-06-01 00:04:13 +0200

    bootstrap, accounts-ui package and iron router

--- fusic.js
+++ fusic.js
@@ -1,19 +1,26 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to fusic.";
-  };
+//ROUTING
+Router.configure({
+  layoutTemplate: 'layout',
+});
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+//mapping van paden naar templates, standaard wordt home template gebruikt en naar "layout" template gestuurd
+Router.map(function() {
+  this.route('home', {
+	path: '/',
+	template: 'home' 
+	});
+});
+
+
+//Client javascript
+
+if (Meteor.isClient) {
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+	//	shouldnt be here anyway, but in a separate file on the server
   });
 }
