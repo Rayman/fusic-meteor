76c0d6cbee64dd45fb359a70fc86f57be353190d
Ramon Wijnands <rayman747@hotmail.com>
2014-06-02 15:44:56 +0200

    add working tabs

--- client/views/playlists.js
+++ client/views/playlists.js
@@ -7,8 +7,19 @@ Template.playlist.selected = function () {
   return Session.equals("playing_song", this._id) ? "selected" : '';
 };
 
-Template.playlist.helpers({
+// initialize active_tab
+Session.set('active_tab', 'songs');
+
+Template.playlistTabs.helpers({
   isActiveTab: function(route) {
-	return Session.equals("active_tab", route) ? "active" : "";
+    return Session.equals("active_tab", route) ? "active" : "";
   }
 });
+
+Template.playlistTabs.events = {
+  'click ul.playlist-tabs > li': function (event) {
+    var li = $(event.currentTarget);
+    var route = li.data('id');
+    Session.set("active_tab", route);
+  }
+};
