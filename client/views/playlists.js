dee421a17b6b6145b6677d668545e58059fada79
Ramon Wijnands <rayman747@hotmail.com>
2014-06-03 21:31:07 +0200

    add working search results

--- client/views/playlists.js
+++ client/views/playlists.js
@@ -17,25 +17,44 @@ Template.playlistTabs.helpers({
 });
 
 Template.playlistTabs.events = {
-  'click ul.playlist-tabs > li': function (event) {
-    var li = $(event.currentTarget);
+  'click ul.playlist-tabs > li': function (e) {
+    var li = $(e.currentTarget);
     var route = li.data('id');
     Session.set("active_tab", route);
+  },
+  'submit form.youtube-search': function (e) {
+    e.preventDefault();
+  },
+  'input input.youtube-query': function (e) {
+    var input = $(e.currentTarget);
+    youtubeSearch(input.val());
   }
 };
 
 // Youtube search results
 
+var searchTimer;
+var searchDelay = 200; // ms
+
+function youtubeSearch(value) {
+  clearTimeout(searchTimer);
+  searchTimer = setTimeout(function() {
+    console.log('youtube search for:', value);
+    youtubeSearchQuery({
+      part: 'snippet',
+      type: 'video',
+      videoEmbeddable: 'true',
+      q: value
+    });
+  },searchDelay);
+}
+
 var searchResults;
 var searchError;
 var searchResultsDependency = new Deps.Dependency();
 
-youtubeSearch = function(options) {
+youtubeSearchQuery = function(options) {
   options = options || {};
-  var defaultOptions = {
-    'part': 'snippet'
-  };
-  options = _.extend(defaultOptions, options);
 
   Meteor.call('youtube_search', options, function(error, data) {
     if (error) {
