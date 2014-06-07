e8bedbef80ebd994c91afcdbd5f49d6e1aa9cd72
Ramon Wijnands <rayman747@hotmail.com>
2014-06-07 20:41:36 +0200

    import changes pepf/fusic-meteor

--- collections.js
+++ collections.js
@@ -5,22 +5,24 @@ Playlists = new Meteor.Collection('playlists', {
     title: {
       type: String,
       label: "Playlist title",
-      max: 100,
+      max: 255,
     },
     cover: {
       type: String,
       label: "Cover url",
       regEx: SimpleSchema.RegEx.Url,
+	  optional: true,
     },
     tags: {
       type: String,
       label: "Tags",
+	  optional: true,
     },
     description: {
       type: String,
       label: "A brief description of your playlist",
       optional: true,
-      max: 1000,
+      max: 1000
     }
   }
 });
