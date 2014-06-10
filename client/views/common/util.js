83970469550f06b76a86fd0de37bbd4959b44c11
Ramon Wijnands <rayman747@hotmail.com>
2014-06-10 21:37:12 +0200

    remove duplicate template helper

--- client/views/common/util.js
+++
@@ -1,6 +0,0 @@
-//Format youtube duration to normal duration
-Handlebars.registerHelper('calcTime', function(oldTime) {
-  //Format PT15M51S --> 15 minutes 51 seconds
-  var time = oldTime || "";
-  return time.replace("PT","").replace("H",":").replace("M",":").replace("S","");
-});
