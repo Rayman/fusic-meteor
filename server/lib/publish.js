661d6340c81e30def3acf823aaa5c2dda32b48f7
Ramon Wijnands <rayman747@hotmail.com>
2014-06-15 23:23:35 +0200

    only publish the songs in the current playlist

--- server/lib/publish.js
+++ server/lib/publish.js
@@ -2,6 +2,9 @@ Meteor.publish("playlists", function () {
   return Playlists.find();
 });
 
-Meteor.publish("songs", function () {
-  return Songs.find();
+Meteor.publish("songs", function (ids) {
+  console.log('publishing songs:', ids);
+  check(ids, [String]);
+
+  return Songs.find({_id: {$in: ids}});
 });
