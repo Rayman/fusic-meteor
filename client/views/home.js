7aac3945370bf240a355dc436ed943c5a47c78c9
Ramon Wijnands <rayman747@hotmail.com>
2014-07-04 00:18:32 +0200

    use a input[type="text"] for the new playlist name

--- client/views/home.js
+++ client/views/home.js
@@ -1,12 +1,12 @@
 Template.home.events = {
   'click button#quickAdd': function (e) {
-	var playlistTitle = $("#playlistName").val();
-    var newList = Playlists.insert({title:playlistTitle}, function(error,id) {
-		//callback, if succeeded, go to new playlist
-		if(id) {
-			Router.go('/playlist/'+id);
-		}
-	});
-  }
+    e.preventDefault(); // no form submit
 
- };
+    var playlistTitle = $("#playlistName").val();
+    var newList = Playlists.insert({title:playlistTitle}, function(error,id) {
+      //callback, if succeeded, go to new playlist
+      if(id)
+        Router.go('/playlist/'+id);
+    });
+  }
+};
