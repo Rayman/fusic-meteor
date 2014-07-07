22f5d52823d72d05aa0a768d22df42e4fd3cc263
Ramon Wijnands <rayman747@hotmail.com>
2014-07-07 22:46:24 +0200

    add playCounts collection

--- collections.js
+++ collections.js
@@ -60,20 +60,54 @@ Playlists = new Meteor.Collection('playlists', {
       optional: true,
       max: 2000
     },
-	
+
 	"songs.$.author": {
-        type: String,	//userId 
+        type: String,	//userId
 		optional: true,
     },
     "songs.$.added": {
         type: Date, 	//Date Object
     },
 	"songs.$.songId": {
-        type: String,	//song Id	
+        type: String,	//song Id
     }
-	
-	
+
+
   }
 });
 
 Songs = new Meteor.Collection('songs');
+
+PlayCounts = new Meteor.Collection('playCounts', {
+  schema: {
+    songId: {
+      type: String,
+    },
+
+    author: {
+      type: String,
+      autoValue: function() {
+        if (this.isInsert) {
+          return this.userId;
+        } else if (this.isUpsert) {
+          return { $setOnInsert: this.userId };
+        } else {
+          this.unset();
+        }
+      }
+    },
+
+    createdAt: {
+      type: Date,
+      autoValue: function() {
+        if (this.isInsert) {
+          return new Date();
+        } else if (this.isUpsert) {
+          return { $setOnInsert: new Date() };
+        } else {
+          this.unset();
+        }
+      }
+    }
+  }
+});
