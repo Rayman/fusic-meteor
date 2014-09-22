7b4a4859ebd10fa45cf37f8319defacddd20ee0a
pepf <pepijnfens@gmail.com>
2014-09-22 23:39:08 +0200

    various tweaks and fixes

--- server/serverMethods.js
+++ server/serverMethods.js
@@ -3,8 +3,16 @@ Meteor.methods({
   getPrivatePlaylists: function () {
     if (!this.userId)
       throw new Meteor.Error(404, "No one is logged in");
-    
+
     //return as static object, just to check if everything works
     return Playlists.find({'owner':this.userId, 'privacy' : 'private'}).fetch();
+  },
+  getLovedSongs: function() {
+    if(!this.userId)
+      throw new Meteor.Error(404, "No one is logged in");
+
+    var user = Meteor.user();
+    //find song info in the cache for each song
+    return Songs.find({_id: {$in: user.profile.lovedSongs} }).fetch();
   }
 });
