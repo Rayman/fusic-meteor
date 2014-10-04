a93c011478d80f3638e9d5e2140b19610f589908
pepf <pepijnfens@gmail.com>
2014-10-04 23:51:57 +0200

    Split playlists.html/js into an overview and the actual playlist view

--- client/views/playlists/playlist.js
+++ client/views/playlists/playlist.js
@@ -1,30 +1,3 @@
-Template.insertPlaylistForm.rendered = function() {
-  Session.setDefault('playlistPrivacy','public');
-  this.find('[data-schema-key=privacy]').value = 'public';
-  $("button").tooltip();
-  AutoForm.hooks({
-    insertPlaylistForm: {
-      onSuccess: function(operation, result, template) {
-        Router.go('/playlist/'+result);
-      }
-    }
-  });
-};
-
-Template.insertPlaylistForm.events = {
-  'click #privacyToggle button' : function(e,template) {
-    var btn = e.currentTarget;
-    Session.set('playlistPrivacy',btn.getAttribute("data-action"));
-    template.find('[data-schema-key=privacy]').value = btn.getAttribute("data-action");
-  }
-};
-
-Template.playlistsEntry.events = {
-  'click .playlist-container' : function(e,template) {
-    Router.go('playlist',{_id:this._id});
-  }
-}
-
 Template.playlist.rendered = function() {
   AutoForm.hooks({
     updatePlaylistForm: { //on successful edit, collapse back out
