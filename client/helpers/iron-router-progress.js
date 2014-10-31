4e55b54185faeb443e5e20d589bd440da00662fd
Ramon Wijnands <rayman747@hotmail.com>
2014-10-31 21:11:24 +0100

    fix the iron router progress configuration

--- client/helpers/iron-router-progress.js
+++ client/helpers/iron-router-progress.js
@@ -1,5 +1,8 @@
-IronRouterProgress.configure({
-  enabled : true,
-  delay: 200,
-  spinner: false,
+// Configure iron-router-progress
+Router.configure({
+  progress        : true,
+  progressDebug   : false,
+  progressDelay   : 150,
+  progressSpinner : false,
+  // progressTick    : true,
 });
