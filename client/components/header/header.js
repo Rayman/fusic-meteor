23a5edbe721348f7e570ead73e73bb7b312bfcb0
Ramon Wijnands <rayman747@hotmail.com>
2014-11-07 21:15:01 +0100

    move the profile link out of the login buttons

--- client/components/header/header.js
+++ client/components/header/header.js
@@ -1,14 +1,3 @@
-Template.header.rendered = function() {
-	//apparantly still the easiest way to make slight adjustments to accounts-ui package
-	setTimeout(function(){
-		var html = '<span class="glyphicon glyphicon-user"></span>';
-    var current = $("#login a.dropdown-toggle").html();
-    html = html + current;
-		$("#login a.dropdown-toggle").html(html);
-	}, 3000);
-
-}
-
-Template._loginButtonsAdditionalLoggedInDropdownActions.user = function(){
+Template.header.user = function (){
   return Meteor.user();
 }
