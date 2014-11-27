4c9c5836a60d02b9905a4de7d89d4844122ee625
pepf <pepijnfens@gmail.com>
2014-11-27 20:03:21 +0100

    Add HypeMachine songs to Suggestions box, changed echonest.js to more general apis.js (including Hype machine)

--- client/components/suggestions/suggestions.js
+++ client/components/suggestions/suggestions.js
@@ -1,44 +1,23 @@
 var refreshSuggestions = function() {
-    Meteor.call('hotRightNow', Session.get("suggestion-genre"), function(error,result) {
+    //hack to make calls to Hypemachine API when selecting this "genre"
+    var apiCall = (Session.get("suggestion-genre") === "hypemPopular") ?  Session.get("suggestion-genre") : "hotRightNow";
+    Meteor.call(apiCall, Session.get("suggestion-genre"), function(error,result) {
     if(error) { console.log(error); return; }
     Session.set("suggestions", result);
   });
-}
+};
 
-Template.suggestions.created = function() {
-  Session.set("suggestion-genre","alternative rock");
-  refreshSuggestions();
-}
-
-Template.suggestions.songs = function() {
-    return Session.get('suggestions');
-}
-
-Template.suggestions.events = {
-  'click button#refreshSuggestions' : refreshSuggestions,
-
-  'click .suggestion-genres li' : function(e) {
-    var genre = e.target.text.toLowerCase();
-    Session.set("suggestion-genre",genre);
-    console.log(e);
-    $(".suggestion-genres li").removeClass("active");
-    $(e.currentTarget).addClass("active");
-    refreshSuggestions();
-  },
-
-  'click #addSong': function (e,template) {
-    var playlistId = Router.current().params._id;
-
-    //prepare query for Youtube
-    var options = {
-      part: 'id',
-      type: 'video',
-      videoEmbeddable: 'true',
-      q: this.artist_name + " " + this.title
-    }
-    //perform Youtube search using artist and song information
-    Meteor.call('youtube_search', options, function(error, data) {
-      if (error) { return false; }
+var addSongfromQuery = function(query, playlistId) {
+  //prepare query for Youtube
+  var options = {
+    part: 'id',
+    type: 'video',
+    videoEmbeddable: 'true',
+    q: query
+  };
+  //perform Youtube search using artist and song information
+  Meteor.call('youtube_search', options, function(error, data) {
+    if (error) { return false; }
 
       //Id from first search result
       var videoId = data.items[0].id.videoId;
@@ -47,7 +26,7 @@ Template.suggestions.events = {
       var options = {
         'part': 'snippet,contentDetails,statistics',
         'id': videoId,
-      }
+      };
       Meteor.call('youtube_videos_list', options);
 
       //Put it in current playlist
@@ -62,6 +41,36 @@ Template.suggestions.events = {
         { $push: { songs: songObject} }
       );
     });
+};
+
+
+Template.suggestions.created = function() {
+  Session.set("suggestion-genre","alternative rock");
+  refreshSuggestions();
+};
+
+Template.suggestions.helpers({
+  "songs" : function() {
+    return Session.get("suggestions");
+  }
+});
+
+Template.suggestions.events = {
+  'click button#refreshSuggestions' : refreshSuggestions,
+
+  'click .suggestion-genres li' : function(e) {
+    var genre = (e.target.getAttribute("data-genre") !== null) ? e.target.getAttribute("data-genre") : e.target.text.toLowerCase();
+    Session.set("suggestion-genre",genre);
+    console.log(e);
+    $(".suggestion-genres li").removeClass("active");
+    $(e.currentTarget).addClass("active");
+    refreshSuggestions();
+  },
+
+  'click #addSong': function (e,template) {
+    var playlistId = Router.current().params._id;
+    addSongfromQuery( this.artist_name + " " + this.title, playlistId);
+
 
   },
-}
+};
