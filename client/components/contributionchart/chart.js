ee172f7dc1abe4262a2621c0a4fcb83cc678836d
pepf <pepijnfens@gmail.com>
2014-12-21 00:11:54 +0100

    change chart color scheme

--- client/components/contributionchart/chart.js
+++ client/components/contributionchart/chart.js
@@ -3,8 +3,7 @@ Template.contributionChart.rendered = function () {
       height = 200,
       radius = Math.min(width, height) / 2;
 
-  var color = d3.scale.ordinal()
-      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+  var color = d3.scale.category20c();
 
   var arc = d3.svg.arc()
       .outerRadius(radius - 10)
